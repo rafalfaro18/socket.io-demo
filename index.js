82192bc3131a2dd77c62c1d75f2bb609f00346b9
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2020-04-08 21:12:50 -0600

    Adds express hello world example

---
+++ index.js
@@ -0,0 +1,11 @@
+// express initializes app to be a function handler that you can supply to an HTTP server.
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
